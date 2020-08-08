8b0a5789782feb937374795bc658a5dfdd616143
Lucifer1113 <kmanish13.gkp@gmail.com>
2020-08-08 20:57:31 +0530

    first commit

---
+++ src/Components/LoginBtn.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './LoginBtn.css';
+
+const LoginBtn =()=>{
+    return(
+        <div className='login-btn'>
+            <img src='' alt=''/>
+            <span>LOGIN WITH FACEBOOK</span>
+        </div>
+        
+    )
+}
+
+export default LoginBtn;
